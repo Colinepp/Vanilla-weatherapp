b28a847773108732fc06389f3472eaa7884c2177
Colinepp <122356026+Colinepp@users.noreply.github.com>
2023-03-09 12:04:53 +0100

    API integration

--- Vanilla-weather-app/src/index.js
+++ Vanilla-weather-app/src/index.js
@@ -1,1 +1,16 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#currentTemp");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+}
+
+let key = "6b4f0f84o6274ffd76c87903428ea4t0";
+let query = "Lisbon";
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
