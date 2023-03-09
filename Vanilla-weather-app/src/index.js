398985f36e47fca1a6529256917bb8f499113cf7
Colinepp <122356026+Colinepp@users.noreply.github.com>
2023-03-09 15:29:52 +0100

    coded date and time

--- Vanilla-weather-app/src/index.js
+++ Vanilla-weather-app/src/index.js
@@ -1,11 +1,39 @@
-function displayTemperature(response) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeather(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   let temperatureElement = document.querySelector("#currentTemp");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let key = "6b4f0f84o6274ffd76c87903428ea4t0";
@@ -13,4 +41,4 @@ let query = "Lisbon";
 let units = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayWeather);
