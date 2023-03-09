481f4037d33bd6ec72a839c83603a2a5f732ca62
Colinepp <122356026+Colinepp@users.noreply.github.com>
2023-03-09 16:23:18 +0100

    add search engine

--- Vanilla-weather-app/src/index.js
+++ Vanilla-weather-app/src/index.js
@@ -34,11 +34,27 @@ function displayWeather(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let iconElement = document.querySelector("#today-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
+}
+function search(city) {
+  let key = "6b4f0f84o6274ffd76c87903428ea4t0";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
-let key = "6b4f0f84o6274ffd76c87903428ea4t0";
-let query = "Lisbon";
-let units = "metric";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
+search("Berlin");
 
-axios.get(apiUrl).then(displayWeather);
+let form = document.querySelector("#searchForm");
+form.addEventListener("submit", handleSubmit);
